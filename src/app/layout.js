62551eb49113b4d291f4a6a0e12f922237431cb7
esbae-dev <es.bae@fastfive.co.kr>
2026-08-07 17:09:57 +0900

    Add admin login icon to top-right of site header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -22,7 +23,30 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header className="flex justify-end px-4 py-3 sm:px-6">
+          <Link
+            href="/admin"
+            aria-label="관리자 로그인"
+            title="관리자 로그인"
+            className="text-zinc-400 hover:text-zinc-700 dark:text-zinc-600 dark:hover:text-zinc-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3H6a3 3 0 0 0-3 3v7.5a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-7.5a3 3 0 0 0-3-3h-.75v-3A5.25 5.25 0 0 0 12 1.5Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
